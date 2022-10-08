fcd482229e929d4718af25b7b37242d64c98ed90
karenlyuan <65468255+karenlyuan@users.noreply.github.com>
2022-10-08 14:07:03 -0700

    Updated Shooter.java

--- Advanced_Robot_Control-main/Advaced_Robot_Control/src/main/java/frc/robot/subsystems/Shooter.java
+++ Advanced_Robot_Control-main/Advaced_Robot_Control/src/main/java/frc/robot/subsystems/Shooter.java
@@ -14,6 +14,10 @@ import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+import edu.wpi.first.networktables.NetworkTableEntry;
+
 
 public class Shooter extends SubsystemBase {
   /** Creates a new Shooter. */
@@ -30,6 +34,16 @@ private final PIDController rightFlywheelPID = new  PIDController(Constants.righ
 private SimpleMotorFeedforward leftFlywheelFF = new  SimpleMotorFeedforward(Constants.leftFlywheelFF.kS, Constants.leftFlywheelFF.kV, Constants.leftFlywheelFF.kA);
 private SimpleMotorFeedforward rightFlywheelFF = new   SimpleMotorFeedforward(Constants.rightFlywheelFF.kS, Constants.rightFlywheelFF.kV, Constants.rightFlywheelFF.kA);
   
+private ShuffleboardTab pidTab = Shuffleboard.getTab("Flywheel PID");
+  private NetworkTableEntry leftFlywheelPIDP = pidTab.add("Left Flywheel PID P", Constants.leftFlywheelPIDConsts.pidP).getEntry();
+  private NetworkTableEntry leftFlywheelPIDI = pidTab.add("Left Flywheel PID I", Constants.leftFlywheelPIDConsts.pidI).getEntry();
+  private NetworkTableEntry leftFlywheelPIDD = pidTab.add("Left Flywheel PID D", Constants.leftFlywheelPIDConsts.pidD).getEntry();
+
+  private NetworkTableEntry rightFlywheelPIDP = pidTab.add("Right Flywheel PID P", Constants.rightFlywheelPIDConsts.pidP).getEntry();
+  private NetworkTableEntry rightFlywheelPIDI = pidTab.add("Right Flywheel PID I", Constants.rightFlywheelPIDConsts.pidI).getEntry();
+  private NetworkTableEntry rightFlywheelPIDD = pidTab.add("Right Flywheel PID D", Constants.rightFlywheelPIDConsts.pidD).getEntry();
+
+
 public Shooter() {
   leftFlywheel.configFactoryDefault();
   leftFlywheel.setInverted(true); leftFlywheel.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Absolute);
@@ -77,6 +91,10 @@ overrideTimer.reset(); // Reset timer
                   
   @Override
   public void periodic() {
+
+    leftFlywheelPID.setPID(leftFlywheelPIDP.getDouble(Constants.leftFlywheelPIDConsts.pidP), leftFlywheelPIDI.getDouble(Constants.leftFlywheelPIDConsts.pidI), leftFlywheelPIDD.getDouble(Constants.leftFlywheelPIDConsts.pidD));
+    rightFlywheelPID.setPID(rightFlywheelPIDP.getDouble(Constants.rightFlywheelPIDConsts.pidP), rightFlywheelPIDI.getDouble(Constants.rightFlywheelPIDConsts.pidI), rightFlywheelPIDD.getDouble(Constants.rightFlywheelPIDConsts.pidD));
+
     // This method will be called once per scheduler run
     SmartDashboard.putNumber("Average RPM", getAverageRPM());
 SmartDashboard.putNumber("Average Current", getFlywheelCurrent());
